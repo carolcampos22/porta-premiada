b2a463fa051fbfce5f2a400fa01972b99d39c9f9
carolcampos22 <mendes.carolina2209@gmail.com>
2024-08-15 18:04:24 -0300

    Validações no número de portas e na  porta com presente

--- pages/game/[doors]/[hasGift].tsx
+++ pages/game/[doors]/[hasGift].tsx
@@ -6,9 +6,19 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 
 export default function game() {
+    const [valid, setValid] = useState(false)
     const [doors, setDoors] = useState([])
 
     const router = useRouter()
+
+    useEffect(() => {
+        const doorsQuantity = +router.query.doors
+        const doorWithGift = +router.query.hasGift
+        const quantityOfValidDoors = doorsQuantity >= 3 && doorsQuantity<= 100
+        const validGiftDoor = doorWithGift >= 1 && doorWithGift <= doorsQuantity
+
+        setValid(quantityOfValidDoors && validGiftDoor)
+    }, [doors])
 
     useEffect(() => {
         const doorsQuantity = +router.query.doors
@@ -25,7 +35,7 @@ export default function game() {
     return (
         <div className={styles.game}>
             <div className={styles.doors}>
-                {renderDoors()}
+                {valid ? renderDoors() : <h1>Valores inválidos!</h1>}
             </div>
             <div className={styles.buttons}>
                 <Link href="/"><button>Reiniciar Jogo</button></Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
+import Link from "next/link";
+import Card from "../components/Card";
+import styles from "../styles/Form.module.css"
+import NumberInput from "../components/NumberInput";
 import { useState } from "react";
-import { Door } from "../components/Door";
-import { Gift } from "../components/Gift";
-import DoorModel from "../model/door";
-import { createDoors, updateDoors } from "../functions/doors";
 
-export default function Home() {
-  
+export default function Form() {
+  const [doorsQuantity, setDoorsQuantity] = useState(3)
+  const [doorsWithGift, setDoorsWithGift] = useState(1)
+
   return (
-    <div style={{display: "flex"}}>
-      <h1>Início</h1>
+    <div className={styles.form}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card>
+          <NumberInput text="Quantidade de Portas?" 
+            value={doorsQuantity}
+            onChange={newQuantity => setDoorsQuantity(newQuantity)}
+          />
+        </Card>
+      </div>
+      <div>
+        <Card>
+        <NumberInput text="Porta com presente?" 
+            value={doorsWithGift}
+            onChange={newDoorsWithGift => setDoorsWithGift(newDoorsWithGift)}
+          />
+        </Card>
+        <Card bgcolor="blueviolet">
+          <Link href={`/game/${doorsQuantity}/${doorsWithGift}`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   )
 }
